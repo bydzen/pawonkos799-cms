1635e0e4b8616f7d4efe065e3c6a5be1408d980b
Sultan Kautsar <42274355+bydzen@users.noreply.github.com>
2022-12-29 21:16:40 +0700

    moving file one path

--- server/backup/do-backup.sh
+++ server/backup/do-backup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "Pawonkos799 Backup Tool"
+sleep 1
 echo "Working..."
-sleep 3
+sleep 1
 
 # Change user as root!
 if ! [ $(id -u) = 0 ]; then
@@ -23,14 +25,33 @@ while true; do
     esac
 done
 
+echo
+
 echo "Exporting database..."
-mysqldump -p -u root pawonkos799 > pawonkos799.sql
+mysqldump -u root -p pawonkos799 > backup-pawonkos799.sql
 echo "Done exporting."
-sleep 3
+sleep 1
 
 echo
 
 echo "Compressing srv file..."
-tar --exclude='backup-file' -cf pawonkos799.tar /srv/www/pawonkos799/*
+tar -cf backup-pawonkos799.tar pawonkos799
 echo "Done compressing."
-sleep 3
+sleep 1
+
+echo
+
+while true; do
+    read -p "Move all to public directory? (y/n): " yn
+    case $yn in
+    [Yy]*)
+        mv pawonkos799.* pawonkos799/other/backup/
+        echo "File moved to pawonkos799/other/backup/---"
+        ;;
+    [Nn]*)
+        printf "\File not move. Done.\n"
+        exit
+        ;;
+    *) printf "Please answer 'Y/y' or 'N/n'.\n" ;;
+    esac
+done
